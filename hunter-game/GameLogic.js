be397a343f4c497f3231ed2886f59f3bac91377b
yih316 <19132023862@163.com>
2024-06-19 20:47:13 +0800

    在GameLogic.js中添加检查碰撞函数，获得随机数函数

--- hunter-game/GameLogic.js
+++ hunter-game/GameLogic.js
@@ -1,3 +1,11 @@
-function getRotation(fromPosition, toPosition) {
+function checkCollision(a, b) {
+    return !((a.y + a.height < b.y)
+             || (a.y > b.y + b.height)
+             || (a.x + a.width < b.x)
+             || (a.x > b.x + b.width))
+}
 
+function getRandom(minimum, maximum){
+    var now = new Date()
+    return Math.floor(Math.random(now.getSeconds()) * (maximum - minimum + 1)) + minimum
 }
